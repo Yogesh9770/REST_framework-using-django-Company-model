be6ad5ce402f436ff743dfaafb9fc51c04866db8
Yogesh <kush.karan9770@gmail.com>
2024-02-22 16:03:59 +0530

    created autorization for django admin panel

--- company_api/api/admin.py
+++ company_api/api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from api.models import Company, Employee
 # Register your models here.
+
+class  CompanyAdmin(admin.ModelAdmin):
+    list_display = ('company_name', 'company_location','company_types')
+    search_fields = ('company_name',)
+
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('employee_first_name','employee_last_name','company')
+    list_filter = ( 'company',)
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Employee,EmployeeAdmin)   
